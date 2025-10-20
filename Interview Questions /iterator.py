30370fe94135d48832d4103cf6f69e307b0e4e7f
kelvinprabhu <your_email@example.com>
2025-10-20 19:35:43 +0530

    iterator

---
+++ Interview Questions /iterator.py
@@ -0,0 +1,9 @@
+# iterator -- object contains countable values 
+# it is used to iterate over iterable objects like list
+# # itratable object --  list ,tuples , dict ,set
+list1 = [1,2,3,4,5]
+l_iter = iter(list1)
+print(next(l_iter))
+print(next(l_iter))
+print(next(l_iter))
+print(next(l_iter))
